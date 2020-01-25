433074b093b223cd6181db5700e97edaee04f1d4
Kenneth Tse <45665576+kennethtse14@users.noreply.github.com>
2020-01-24 21:27:16 -0500

    Add files via upload

--- mycp.cpp
+++ mycp.cpp
@@ -1,39 +1,48 @@
-int main(int argc, char const *argv[])
-{
- 	ifstream fin;
- 	ofstream fout;
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	
+	ifstream fin;
+	ofstream fout;
 	
 	// check if there are enough arguments
+	int i = 0;
 	
+	if(argv[i]!=NULL){
+		cout << "user provides enough arguments" << endl;
+	}
+	else{
+		cout << "user did not provide enough arguments" << endl;
+	}
 	
-	// open the first file
- 	
+	// open first file
+	fin.open("file1.txt");
+	
 	char c;
-
- 	if (fin.fail()) // check if it is successful 
- 	{
- 		cerr << " Cannot open the input file!" << endl;
- 		return 1;
- 	}
- 	
-
-	// open the second file
 	
- 	if (fout.fail())
- 	{
- 		cerr << " Cannot open the output file!" << endl;
- 		return 1;
- 	}
- 	
- 	while(fin.get(c)) 
-	{
+	if (fin.fail()){
+		cerr << " Cannot open the input file!" << endl;
+		return 1;
+	}
+
+	// opoen second file
+	fout.open("file2.txt");
+	
+	if(fout.fail()){
+		cerr << "Cannot open the output file!" << endl;
+		return 1;
+	
+	}
+	
+	while (fin.get(c)){
 		fout << c;
 	}
- 	
- 	fin.close(); 
 
- 	fout.close();
+	fin.close();
+	fout.close();
 
- 	 return 0;
-
- } 
+	return 0;
+}
